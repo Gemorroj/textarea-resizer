96d040ea4e800572c6ba763a842bd67149877c9e
Gemorroj <wapinet@gmail.com>
2013-09-23 17:38:29 +0300

    refactoring webkit code

--- webkit/includes/textarea_resizer.js
+++ webkit/includes/textarea_resizer.js
@@ -1,193 +1,204 @@
-opera.isReady(function(){
 "use strict";
-window.addEventListener('DOMContentLoaded', function() {
-  var preferences = widget.preferences;
-  var markerWidth = 10, markerHeight = 10, markerTimeout = 1200, move = false, removeMarkerTimeout = 0, current = null, marker = null, config = {
-    pressModifierKey: preferences.pressModifierKey,
-    markerMinWidth: preferences.markerMinWidth,
-    markerMinHeight: preferences.markerMinHeight,
-    textarea: preferences.textarea === '1',
-    iframe: preferences.iframe === '1',
-    select: preferences.select === '1',
-    img: preferences.img === '1',
-    video: preferences.video === '1',
-    canvas: preferences.canvas === '1',
-    objectEmbed: preferences.objectEmbed === '1',
-    imgMinWidth: preferences.imgMinWidth,
-    imgMinHeight: preferences.imgMinHeight,
-    input: preferences.input === '1',
-    inputText: preferences.inputText === '1',
-    inputPassword: preferences.inputPassword === '1',
-    inputFile: preferences.inputFile === '1',
-    inputEmail: preferences.inputEmail === '1',
-    inputSearch: preferences.inputSearch === '1',
-    inputTel: preferences.inputTel === '1',
-    inputUrl: preferences.inputUrl === '1',
-    textareaDblclick: preferences.textareaDblclick === '1',
-    iframeDblclick: preferences.iframeDblclick === '1',
-    imgDblclick: preferences.imgDblclick === '1',
-    inputDblclick: preferences.inputDblclick === '1',
-    selectDblclick: preferences.selectDblclick === '1',
-    videoDblclick: preferences.videoDblclick === '1',
-    canvasDblclick: preferences.canvasDblclick === '1',
-    objectEmbedDblclick: preferences.objectEmbedDblclick === '1'
-  }, _docBody = document.body, _docElement = document.documentElement;
-  if (!(_docElement instanceof window.HTMLHtmlElement)) {
-    return;
-  }
-  function getOffset(obj) {
+
+var markerWidth = 10,
+markerHeight = 10,
+markerTimeout = 1200,
+move = false,
+removeMarkerTimeout = 0,
+current = null,
+marker = null,
+config = {},
+_docBody = document.body,
+_docElement = document.documentElement;
+
+
+function getOffset(obj) {
     var offset = {
-      top: obj.offsetTop,
-      left: obj.offsetLeft
-    }, parent = obj.offsetParent;
+        top: obj.offsetTop,
+        left: obj.offsetLeft
+    },
+    parent = obj.offsetParent;
     while (null !== parent) {
-      offset.top += parent.offsetTop;
-      offset.left += parent.offsetLeft;
-      parent = parent.offsetParent;
+        offset.top += parent.offsetTop;
+        offset.left += parent.offsetLeft;
+        parent = parent.offsetParent;
     }
     return offset;
-  }
-  function isHidden(obj) {
+}
+function isHidden(obj) {
     while (obj && obj !== _docBody && obj !== _docElement) {
-      var style = window.getComputedStyle(obj, null);
-      if (style.display === 'none' || style.visibility === 'hidden' || style.opacity === "0") {
-        return true;
-      }
-      obj = obj.parentElement;
+        var style = window.getComputedStyle(obj, null);
+        if (style.display === 'none' || style.visibility === 'hidden' || style.opacity === "0") {
+            return true;
+        }
+        obj = obj.parentElement;
     }
     return false;
-  }
-  function isFixed(obj) {
+}
+function isFixed(obj) {
     while (obj && obj !== _docBody && obj !== _docElement) {
-      if (window.getComputedStyle(obj, null).position === 'fixed') {
-        return true;
-      }
-      obj = obj.parentElement;
+        if (window.getComputedStyle(obj, null).position === 'fixed') {
+            return true;
+        }
+        obj = obj.parentElement;
     }
     return false;
-  }
-  function calculatePosition() {
+}
+function calculatePosition() {
     if (!current) {
       return [0,0];
     }
     var offset = getOffset(current);
-    return [offset.left + current.offsetWidth - markerWidth,offset.top + current.offsetHeight - markerHeight];
-  }
-  function updatePosition() {
+    return [offset.left + current.offsetWidth - markerWidth, offset.top + current.offsetHeight - markerHeight];
+}
+function updatePosition() {
     if (null === marker) {
-      return;
+        return;
     }
     var pos = calculatePosition();
     switch (isFixed(current) === true) {
-      case true:
-        marker.style.position = 'fixed';
-        marker.style.left = (pos[0] - window.pageXOffset) + 'px';
-        marker.style.top = (pos[1] - window.pageYOffset) + 'px';
-        break;
-      case false:
-        marker.style.position = 'absolute';
-        marker.style.left = pos[0] + 'px';
-        marker.style.top = pos[1] + 'px';
-        break;
+        case true:
+            marker.style.position = 'fixed';
+            marker.style.left = (pos[0] - window.pageXOffset) + 'px';
+            marker.style.top = (pos[1] - window.pageYOffset) + 'px';
+            break;
+        case false:
+            marker.style.position = 'absolute';
+            marker.style.left = pos[0] + 'px';
+            marker.style.top = pos[1] + 'px';
+            break;
     }
-  }
-  function onMouseOut() {
+}
+function onMouseOut() {
     if (move === false) {
-      removeMarkerTimeout = window.setTimeout(detachResizeMarker, markerTimeout);
+        removeMarkerTimeout = window.setTimeout(detachResizeMarker, markerTimeout);
     }
-  }
-  function onMarkerMouseMove(e) {
+}
+function onMarkerMouseMove(e) {
     if (move === true && current && e) {
-      var offset = getOffset(current);
-      var tag = current.tagName.toUpperCase(), w = e.clientX - offset.left + window.pageXOffset, h = e.clientY - offset.top + window.pageYOffset;
-      if (w > config.markerMinWidth) {
-        current.style.width = w + 'px !important';
-      }
-      if (h > config.markerMinHeight && (tag === 'TEXTAREA' || tag === 'IMG' || tag === 'IFRAME' || tag === 'VIDEO' || tag === 'OBJECT' || tag === 'EMBED' || tag === 'CANVAS' || (tag === 'SELECT' && current.multiple === true))) {
-        current.style.height = h + 'px !important';
-      }
-      updatePosition();
+        var offset = getOffset(current);
+        var tag = current.tagName.toUpperCase(), w = e.clientX - offset.left + window.pageXOffset, h = e.clientY - offset.top + window.pageYOffset;
+        if (w > config.markerMinWidth) {
+            current.style.width = w + 'px !important';
+        }
+        if (h > config.markerMinHeight && (tag === 'TEXTAREA' || tag === 'IMG' || tag === 'IFRAME' || tag === 'VIDEO' || tag === 'OBJECT' || tag === 'EMBED' || tag === 'CANVAS' || (tag === 'SELECT' && current.multiple === true))) {
+            current.style.height = h + 'px !important';
+        }
+        updatePosition();
     }
-  }
-  function onMarkerMouseUp(e) {
+}
+function onMarkerMouseUp(e) {
     move = false;
     if (!e.target || (e.target !== current && e.target !== marker)) {
-      removeMarkerTimeout = window.setTimeout(detachResizeMarker, markerTimeout);
+        removeMarkerTimeout = window.setTimeout(detachResizeMarker, markerTimeout);
     }
-  }
-  function detachResizeMarker() {
+}
+function detachResizeMarker() {
     if (null !== marker) {
-      marker.parentNode.removeChild(marker);
+        marker.parentNode.removeChild(marker);
     }
     if (current) {
-      current.removeEventListener('mouseout', onMouseOut, false);
+        current.removeEventListener('mouseout', onMouseOut, false);
     }
     current = null;
     move = false;
     marker = null;
     document.removeEventListener('mousemove', onMarkerMouseMove, false);
     document.removeEventListener('mouseup', onMarkerMouseUp, false);
-  }
-  function attachResizeMarker(obj) {
+}
+function attachResizeMarker(obj) {
     if (move === true || !obj || obj === current) {
-      return;
+        return;
     }
     detachResizeMarker();
     current = obj;
     var tag = obj.tagName.toUpperCase(), m = document.createElement('div'), pos = calculatePosition();
     m.setAttribute('style', 'left: ' + pos[0] + 'px;' + 'top: ' + pos[1] + 'px;' + 'width: ' + markerWidth + 'px;' + 'height: ' + markerHeight + 'px;' + 'cursor: se-resize;' + 'border: none;' + 'position: absolute;' + 'z-index: 2147483647;' + 'padding: 0;' + 'margin: 0;' + 'background: transparent url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs%2B9AAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAXklEQVQYlY3OwQmAMBAAwU2sIITUoGAZl2IvkMZEbOH0I%2BJDvOx7HhuMj8xo2gGoIuSSzjiCmnbiCAJe8AdVkRs6KJdEHEFNO9Myry56Hj1URQj7drgol0QwMxc17VzVhljiijeJtgAAAABJRU5ErkJggg%3D%3D") no-repeat 50% 50%;');
     if ((tag === 'TEXTAREA' && config.textareaDblclick === true) || (tag === 'IMG' && config.imgDblclick === true) || (tag === 'INPUT' && config.inputDblclick === true) || (tag === 'SELECT' && config.selectDblclick === true) || (tag === 'IFRAME' && config.iframeDblclick === true) || (tag === 'VIDEO' && config.videoDblclick === true) || (tag === 'CANVAS' && config.canvasDblclick === true) || ((tag === 'OBJECT' || tag === 'EMBED') && config.objectEmbedDblclick === true)) {
-      var style = window.getComputedStyle(obj, null);
-      obj.defaultWidth = (obj.defaultWidth ? obj.defaultWidth : style.width);
-      obj.defaultHeight = (obj.defaultHeight ? obj.defaultHeight : style.height);
-      m.addEventListener('dblclick', function() {
-        current.style.width = obj.defaultWidth;
-        current.style.height = obj.defaultHeight;
-        onMouseOut();
-      }, false);
+        var style = window.getComputedStyle(obj, null);
+        obj.defaultWidth = (obj.defaultWidth ? obj.defaultWidth : style.width);
+        obj.defaultHeight = (obj.defaultHeight ? obj.defaultHeight : style.height);
+        m.addEventListener('dblclick', function () {
+            current.style.width = obj.defaultWidth;
+            current.style.height = obj.defaultHeight;
+            onMouseOut();
+        }, false);
     }
-    m.addEventListener('mousedown', function(e) {
-      e.preventDefault();
-      move = true;
+    m.addEventListener('mousedown', function (e) {
+        e.preventDefault();
+        move = true;
     }, false);
-    m.addEventListener('mouseup', function() {
-      move = false;
+    m.addEventListener('mouseup', function () {
+        move = false;
     }, false);
-    m.addEventListener('mouseout', function() {
-      onMouseOut();
+    m.addEventListener('mouseout', function () {
+        onMouseOut();
     }, false);
     current.addEventListener('mouseout', onMouseOut, false);
     document.addEventListener('mousemove', onMarkerMouseMove, false);
     document.addEventListener('mouseup', onMarkerMouseUp, false);
     marker = _docElement.appendChild(m);
     updatePosition();
-  }
-  document.addEventListener('mouseover', function(e) {
-    var target = e.target;
-    if (target) {
-      var modifierKey = true, tag = target.tagName.toUpperCase();
-      switch (config.pressModifierKey) {
-        case '1':
-          modifierKey = e.shiftKey === true;
-          break;
-        case '2':
-          modifierKey = e.ctrlKey === true;
-          break;
-        case '3':
-          modifierKey = e.altKey === true;
-          break;
-      }
-      if ((typeof target.style !== 'undefined' && target.style.resize !== 'none') && modifierKey && ((tag === 'TEXTAREA' && config.textarea === true) || (tag === 'SELECT' && config.select === true) || (tag === 'OPTION' && config.select === true && (tag = 'SELECT', target = target.parentElement)) || (tag === 'IFRAME' && config.iframe === true) || (tag === 'IMG' && config.img === true && target.width > config.imgMinWidth && target.height > config.imgMinHeight) || (tag === 'VIDEO' && config.video === true) || (tag === 'CANVAS' && config.canvas === true) || ((tag === 'OBJECT' || tag === 'EMBED') && config.objectEmbed === true) || (tag === 'INPUT' && config.input === true && (config.inputText === true && target.type === 'text' || config.inputPassword === true && target.type === 'password' || config.inputFile === true && target.type === 'file' || config.inputEmail === true && target.type === 'email' || config.inputSearch === true && target.type === 'search' || config.inputTel === true && target.type === 'tel' || config.inputUrl === true && target.type === 'url'))) && (isHidden(target) === false)) {
-        attachResizeMarker(target);
-        if (target === current) {
-          window.clearTimeout(removeMarkerTimeout);
-        }
-      } else if (target === marker) {
-        window.clearTimeout(removeMarkerTimeout);
-      } else if (move === false) {
-        detachResizeMarker();
-      }
-    }
-  }, false);
-}, false);
-});
+}
+
+if (_docElement instanceof window.HTMLHtmlElement) {
+    chrome.storage.sync.get([
+        'pressModifierKey',
+        'markerMinWidth',
+        'markerMinHeight',
+        'textarea',
+        'iframe',
+        'select',
+        'img',
+        'video',
+        'canvas',
+        'objectEmbed',
+        'imgMinWidth',
+        'imgMinHeight',
+        'input',
+        'inputText',
+        'inputPassword',
+        'inputFile',
+        'inputEmail',
+        'inputSearch',
+        'inputTel',
+        'inputUrl',
+        'textareaDblclick',
+        'iframeDblclick',
+        'imgDblclick',
+        'inputDblclick',
+        'selectDblclick',
+        'videoDblclick',
+        'canvasDblclick',
+        'objectEmbedDblclick'
+    ], function (tmp) {
+        config = tmp;
+        document.addEventListener('mouseover', function (e) {
+            var target = e.target;
+            if (target) {
+                var modifierKey = true, tag = target.tagName.toUpperCase();
+                switch (config.pressModifierKey) {
+                    case 1:
+                        modifierKey = e.shiftKey === true;
+                        break;
+                    case 2:
+                        modifierKey = e.ctrlKey === true;
+                        break;
+                    case 3:
+                        modifierKey = e.altKey === true;
+                        break;
+                }
+                if ((typeof target.style !== 'undefined' && target.style.resize !== 'none') && modifierKey && ((tag === 'TEXTAREA' && config.textarea === true) || (tag === 'SELECT' && config.select === true) || (tag === 'OPTION' && config.select === true && (tag = 'SELECT', target = target.parentElement)) || (tag === 'IFRAME' && config.iframe === true) || (tag === 'IMG' && config.img === true && target.width > config.imgMinWidth && target.height > config.imgMinHeight) || (tag === 'VIDEO' && config.video === true) || (tag === 'CANVAS' && config.canvas === true) || ((tag === 'OBJECT' || tag === 'EMBED') && config.objectEmbed === true) || (tag === 'INPUT' && config.input === true && (config.inputText === true && target.type === 'text' || config.inputPassword === true && target.type === 'password' || config.inputFile === true && target.type === 'file' || config.inputEmail === true && target.type === 'email' || config.inputSearch === true && target.type === 'search' || config.inputTel === true && target.type === 'tel' || config.inputUrl === true && target.type === 'url'))) && (isHidden(target) === false)) {
+                    attachResizeMarker(target);
+                    if (target === current) {
+                        window.clearTimeout(removeMarkerTimeout);
+                    }
+                } else if (target === marker) {
+                    window.clearTimeout(removeMarkerTimeout);
+                } else if (move === false) {
+                    detachResizeMarker();
+                }
+            }
+        }, false);
+    });
+}
--- webkit/options.js
+++ webkit/options.js
@@ -1,75 +1,101 @@
-opera.isReady(function(){
-var preferences = window["preferences"] = widget.preferences;
-function saveOptions() {
-  try {
-    preferences.setItem('pressModifierKey', document.getElementById('pressModifierKey').selectedIndex);
-    preferences.setItem('markerMinWidth', document.getElementById('markerMinWidth').value);
-    preferences.setItem('markerMinHeight', document.getElementById('markerMinHeight').value);
-    preferences.setItem('textarea', document.getElementById('textarea').checked ? '1' : '0');
-    preferences.setItem('iframe', document.getElementById('iframe').checked ? '1' : '0');
-    preferences.setItem('select', document.getElementById('select').checked ? '1' : '0');
-    preferences.setItem('img', document.getElementById('img').checked ? '1' : '0');
-    preferences.setItem('video', document.getElementById('video').checked ? '1' : '0');
-    preferences.setItem('canvas', document.getElementById('canvas').checked ? '1' : '0');
-    preferences.setItem('objectEmbed', document.getElementById('objectEmbed').checked ? '1' : '0');
-    preferences.setItem('imgMinWidth', document.getElementById('imgMinWidth').value);
-    preferences.setItem('imgMinHeight', document.getElementById('imgMinHeight').value);
-    preferences.setItem('input', document.getElementById('input').checked ? '1' : '0');
-    preferences.setItem('inputText', document.getElementById('inputText').checked ? '1' : '0');
-    preferences.setItem('inputPassword', document.getElementById('inputPassword').checked ? '1' : '0');
-    preferences.setItem('inputFile', document.getElementById('inputFile').checked ? '1' : '0');
-    preferences.setItem('inputEmail', document.getElementById('inputEmail').checked ? '1' : '0');
-    preferences.setItem('inputSearch', document.getElementById('inputSearch').checked ? '1' : '0');
-    preferences.setItem('inputTel', document.getElementById('inputTel').checked ? '1' : '0');
-    preferences.setItem('inputUrl', document.getElementById('inputUrl').checked ? '1' : '0');
-    preferences.setItem('textareaDblclick', document.getElementById('textareaDblclick').checked ? '1' : '0');
-    preferences.setItem('iframeDblclick', document.getElementById('iframeDblclick').checked ? '1' : '0');
-    preferences.setItem('imgDblclick', document.getElementById('imgDblclick').checked ? '1' : '0');
-    preferences.setItem('inputDblclick', document.getElementById('inputDblclick').checked ? '1' : '0');
-    preferences.setItem('selectDblclick', document.getElementById('selectDblclick').checked ? '1' : '0');
-    preferences.setItem('videoDblclick', document.getElementById('videoDblclick').checked ? '1' : '0');
-    preferences.setItem('canvasDblclick', document.getElementById('canvasDblclick').checked ? '1' : '0');
-    preferences.setItem('objectEmbedDblclick', document.getElementById('objectEmbedDblclick').checked ? '1' : '0');
-  } catch (e) {
-    document.getElementById('response').innerHTML = '<span class="error">Error! (' + e + ')</span>';
-    return false;
-  }
-  document.getElementById('response').innerHTML = '<span class="ok">Options Saved!</span>';
-  var interval = window.setInterval(function() {
-    document.getElementById('response').innerHTML = '';
-    window.clearInterval(interval);
-  }, 10000);
-  return false;
-}
-var saveOptions = window["saveOptions"] = saveOptions;
+"use strict";
+
 window.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('pressModifierKey').selectedIndex = preferences.pressModifierKey;
-  document.getElementById('markerMinWidth').value = preferences.markerMinWidth;
-  document.getElementById('markerMinHeight').value = preferences.markerMinHeight;
-  document.getElementById('textarea').checked = preferences.textarea === '1';
-  document.getElementById('iframe').checked = preferences.iframe === '1';
-  document.getElementById('select').checked = preferences.select === '1';
-  document.getElementById('img').checked = preferences.img === '1';
-  document.getElementById('video').checked = preferences.video === '1';
-  document.getElementById('canvas').checked = preferences.canvas === '1';
-  document.getElementById('objectEmbed').checked = preferences.objectEmbed === '1';
-  document.getElementById('imgMinWidth').value = preferences.imgMinWidth;
-  document.getElementById('imgMinHeight').value = preferences.imgMinHeight;
-  document.getElementById('input').checked = preferences.input === '1';
-  document.getElementById('inputText').checked = preferences.inputText === '1';
-  document.getElementById('inputPassword').checked = preferences.inputPassword === '1';
-  document.getElementById('inputFile').checked = preferences.inputFile === '1';
-  document.getElementById('inputEmail').checked = preferences.inputEmail === '1';
-  document.getElementById('inputSearch').checked = preferences.inputSearch === '1';
-  document.getElementById('inputTel').checked = preferences.inputTel === '1';
-  document.getElementById('inputUrl').checked = preferences.inputUrl === '1';
-  document.getElementById('textareaDblclick').checked = preferences.textareaDblclick === '1';
-  document.getElementById('iframeDblclick').checked = preferences.iframeDblclick === '1';
-  document.getElementById('imgDblclick').checked = preferences.imgDblclick === '1';
-  document.getElementById('inputDblclick').checked = preferences.inputDblclick === '1';
-  document.getElementById('selectDblclick').checked = preferences.selectDblclick === '1';
-  document.getElementById('videoDblclick').checked = preferences.videoDblclick === '1';
-  document.getElementById('canvasDblclick').checked = preferences.canvasDblclick === '1';
-  document.getElementById('objectEmbedDblclick').checked = preferences.objectEmbedDblclick === '1';
+    chrome.storage.sync.get([
+        'pressModifierKey',
+        'markerMinWidth',
+        'markerMinHeight',
+        'textarea',
+        'iframe',
+        'select',
+        'img',
+        'video',
+        'canvas',
+        'objectEmbed',
+        'imgMinWidth',
+        'imgMinHeight',
+        'input',
+        'inputText',
+        'inputPassword',
+        'inputFile',
+        'inputEmail',
+        'inputSearch',
+        'inputTel',
+        'inputUrl',
+        'textareaDblclick',
+        'iframeDblclick',
+        'imgDblclick',
+        'inputDblclick',
+        'selectDblclick',
+        'videoDblclick',
+        'canvasDblclick',
+        'objectEmbedDblclick'
+    ], function (items) {
+        console.log(items);
+        document.getElementById('pressModifierKey').selectedIndex = items.pressModifierKey;
+        document.getElementById('markerMinWidth').value = items.markerMinWidth;
+        document.getElementById('markerMinHeight').value = items.markerMinHeight;
+        document.getElementById('textarea').checked = items.textarea;
+        document.getElementById('iframe').checked = items.iframe;
+        document.getElementById('select').checked = items.select;
+        document.getElementById('img').checked = items.img;
+        document.getElementById('video').checked = items.video;
+        document.getElementById('canvas').checked = items.canvas;
+        document.getElementById('objectEmbed').checked = items.objectEmbed;
+        document.getElementById('imgMinWidth').value = items.imgMinWidth;
+        document.getElementById('imgMinHeight').value = items.imgMinHeight;
+        document.getElementById('input').checked = items.input;
+        document.getElementById('inputText').checked = items.inputText;
+        document.getElementById('inputPassword').checked = items.inputPassword;
+        document.getElementById('inputFile').checked = items.inputFile;
+        document.getElementById('inputEmail').checked = items.inputEmail;
+        document.getElementById('inputSearch').checked = items.inputSearch;
+        document.getElementById('inputTel').checked = items.inputTel;
+        document.getElementById('inputUrl').checked = items.inputUrl;
+        document.getElementById('textareaDblclick').checked = items.textareaDblclick;
+        document.getElementById('iframeDblclick').checked = items.iframeDblclick;
+        document.getElementById('imgDblclick').checked = items.imgDblclick;
+        document.getElementById('inputDblclick').checked = items.inputDblclick;
+        document.getElementById('selectDblclick').checked = items.selectDblclick;
+        document.getElementById('videoDblclick').checked = items.videoDblclick;
+        document.getElementById('canvasDblclick').checked = items.canvasDblclick;
+        document.getElementById('objectEmbedDblclick').checked = items.objectEmbedDblclick;
+    });
+
+    document.getElementById("form").addEventListener("submit", function () {
+        chrome.storage.sync.set({
+            'pressModifierKey': document.getElementById('pressModifierKey').selectedIndex,
+            'markerMinWidth': window.parseInt(document.getElementById('markerMinWidth').value, 10),
+            'markerMinHeight': window.parseInt(document.getElementById('markerMinHeight').value, 10),
+            'textarea': document.getElementById('textarea').checked,
+            'iframe': document.getElementById('iframe').checked,
+            'select': document.getElementById('select').checked,
+            'img': document.getElementById('img').checked,
+            'video': document.getElementById('video').checked,
+            'canvas': document.getElementById('canvas').checked,
+            'objectEmbed': document.getElementById('objectEmbed').checked,
+            'imgMinWidth': window.parseInt(document.getElementById('imgMinWidth').value, 10),
+            'imgMinHeight': window.parseInt(document.getElementById('imgMinHeight').value, 10),
+            'input': document.getElementById('input').checked,
+            'inputText': document.getElementById('inputText').checked,
+            'inputPassword': document.getElementById('inputPassword').checked,
+            'inputFile': document.getElementById('inputFile').checked,
+            'inputEmail': document.getElementById('inputEmail').checked,
+            'inputSearch': document.getElementById('inputSearch').checked,
+            'inputTel': document.getElementById('inputTel').checked,
+            'inputUrl': document.getElementById('inputUrl').checked,
+            'textareaDblclick': document.getElementById('textareaDblclick').checked,
+            'iframeDblclick': document.getElementById('iframeDblclick').checked,
+            'imgDblclick': document.getElementById('imgDblclick').checked,
+            'inputDblclick': document.getElementById('inputDblclick').checked,
+            'selectDblclick': document.getElementById('selectDblclick').checked,
+            'videoDblclick': document.getElementById('videoDblclick').checked,
+            'canvasDblclick': document.getElementById('canvasDblclick').checked,
+            'objectEmbedDblclick': document.getElementById('objectEmbedDblclick').checked
+        }, function () {
+            document.getElementById('response').innerHTML = '<span class="ok">Options Saved!</span>';
+        });
+
+        return false;
+    }, false);
 }, false);
-});
